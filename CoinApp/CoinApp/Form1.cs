15bd443c2715a5aac0ddfbe53a2879b3bc38373b
Jmem716 <jmem716@gmail.com>
2018-08-07 03:02:54 -0400

    Simple program that shows what side of quarter user specifies.

---
+++ CoinApp/CoinApp/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CoinApp
+{
+    public partial class CoinApp : Form
+    {
+        public CoinApp()
+        {
+            InitializeComponent();
+        }
+
+        private void heads_Click(object sender, EventArgs e)
+        {
+            headsPictureBox.Visible = true;
+            tailsPictureBox.Visible = false;
+        }
+
+        private void headsPictureBox_Click(object sender, EventArgs e)
+        {
+           
+
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tails_Click(object sender, EventArgs e)
+        {
+            tailsPictureBox.Visible = true;
+            headsPictureBox.Visible = false;
+        }
+    }
+}
